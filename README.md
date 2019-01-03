659d59c2e9225a79a6e68be8bd90a58ddf2bcb2f
jin kind <jinkind_ho@mymail.sutd.edu.sg>
2019-01-03 13:46:32 +0800

    project kotlin2. using async task in kotlin

--- README.md
+++
@@ -1,6 +0,0 @@
-# KotlinProjects
-These are my kotlin project which I have been working on
- 
- ## first project
-use of basic functions, on click listener. Wanted o set on click function in XML, triggering a function in activity,  but failed.
-second activity uses anko. without creating XML file. using a second file caled SecondActivityUI, create all the UI components. Achieveing same functionality as XML
---
+++ kotlin2/app/src/main/java/com/example/hojin/kotlin2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.hojin.kotlin2
+
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        estimatePi().execute(10)
+    }
+    class estimatePi : AsyncTask<Int, Unit, String>() {
+
+        override protected fun doInBackground(vararg params: Int?): String {
+            var a :Int? = params!![0]
+            //is there an easier way to do this?
+            for (i in 0..a!!) {
+                a += i!!
+                Log.i("hi",a.toString())
+            }
+
+            return "hi"
+        }
+
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.i("beye","hi")
+        }
+    }
+}
+
+private operator fun Unit.rangeTo(unit: Unit) {}
