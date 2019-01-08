5faf575a48eccedc815f755d23a9ef765ac63c3c
jin kind <jinkind_ho@mymail.sutd.edu.sg>
2019-01-08 15:14:26 +0800

    commit for kotin4, using asynctask with WeatherAPI to get longtitude, lattitude of a city, to display on textview

---
+++ kotlin4/app/src/main/java/com/example/hojin/kotlin4/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.hojin.kotlin4
+
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64InputStream
+import android.util.Log
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.setContentView
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpCookie
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var mainUI = MainActivityUI()
+        mainUI.setContentView(this)
+
+    }
+
+    fun QueryYahoo(ui: AnkoContext<MainActivity>, city: CharSequence?): String {
+        Log.i("this is main",String.format("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=c151c3e1a3f3a69e94d9f40d5a08a278", city))
+        var urlTemplate = String.format("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=c151c3e1a3f3a69e94d9f40d5a08a278", city)
+        var finalString = MyAsyncTask().execute(urlTemplate).get()
+        return finalString[1]
+    }
+    class MyAsyncTask: AsyncTask<String?, Unit, List<String>>() {
+        override protected fun doInBackground(vararg p0: String?): List<String> {
+
+                val url = URL(p0[0])
+            Log.i("this is main", url.toString())
+
+            val urlConnect = url.openConnection() as HttpURLConnection
+            urlConnect.connectTimeout=7000
+            var inString =  ConvertStreamToString(urlConnect.inputStream)
+
+            Log.i("success",inString)
+            var json = JSONObject(inString)
+            val sys1 = json.getJSONObject("sys")
+            val sunrise = sys1.getString("sunrise")
+            val coord = json.getJSONObject("coord")
+            val long = coord.getString("lon")
+            val lat = coord.getString("lat")
+            val lis1 = listOf<String>(sunrise,long,lat)
+            Log.i("success",sunrise)
+            return lis1
+
+        }
+
+
+
+        fun ConvertStreamToString(inputStream: InputStream):String {
+            val bufferReader = BufferedReader(InputStreamReader(inputStream))
+            var line :String
+            var AllString: String=""
+            try {
+                do{
+                    line = bufferReader.readLine()
+                    if (line!=null){AllString+=line
+                    }
+
+                }while(line!=null)
+                inputStream.close()
+            }catch (ex:Exception){}
+            return  AllString
+        }
+    }
+}
---
+++ kotlin4/app/src/main/java/com/example/hojin/kotlin4/MainActivityUI.kt
@@ -0,0 +1,71 @@
+package com.example.hojin.kotlin4
+
+/**
+ * Created by hojin on 7/1/2019.
+ */
+
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.EditText
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.w3c.dom.Text
+
+class MainActivityUI : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>)= with(ui) {
+        val ID_editText=1
+        val ID_textview = 2
+        val ID_button1 = 3
+        val ID_button2 = 4
+        var editText1: EditText
+
+        relativeLayout{
+            editText1 = editText{
+                id = ID_editText
+                hint = "enter city"
+
+
+            }.lparams(width = matchParent){
+                margin= dip(20)
+                topMargin = dip (20)}
+            var tv =textView{
+                id= ID_textview
+                textSize = 20.0f
+                setText("i am a string")
+            }.lparams(width = wrapContent){
+                gravity = Gravity.CENTER_HORIZONTAL
+                below(ID_editText)
+
+                }
+            button{
+                setText("ok")
+                id = ID_button1
+                width = wrapContent
+                setOnClickListener{
+                    Log.i("this is UI",editText1.text.toString())
+
+                    var returnVar=ui.owner.QueryYahoo(ui,editText1.text.toString())
+                    tv.text = returnVar.toString()
+                }
+            }.lparams{
+                below(ID_textview)
+
+            }
+            button{
+                setText("ok")
+                id = ID_button2
+                width = wrapContent
+            }.lparams{
+                rightOf(ID_button1)
+                below(ID_textview)
+            }
+
+            }
+
+
+
+
+    }
+
+}
